323e48c254cd9dace50a20601b8d9ca6dd68bf23
Igor Tyshchenko <igor@tyshchenko.me>
2024-03-29 13:21:19 +0500

    Добавьте файлы проекта.

---
+++ chess.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+    char board[] = { 'a', 'b', 'c', 'd', 'e','f','g','h'};
+    std::cout << " ";
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << " " << board[i] << " ";
+
+    }
+    std::cout << "\n";
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << " " << 8 - i << " ";
+        for (int j = 0; j < 8; j++)
+        {
+            std::cout << board[j] << 8 - i << " ";
+        }
+        std::cout << "\n";
+        std::cout << " ";
+        for (int j = 0; j < 0; j++)
+        {
+            std::cout << "-------";
+        }
+        std::cout << "-" << "\n";
+    }
+}
+
